0b81c5b55e9569c2f851131eb30b957a6eb9bdab
Nathan Skiles <nateskiles94@gmail.com>
2021-08-11 12:07:00 -0700

    init

---
+++ js/booleanArrayCount.js
@@ -0,0 +1,18 @@
+// Create a function which returns the number of true values there are in an array.
+// function countTrue(arr) {
+//     let count = 0;
+//     arr.forEach(bool => {
+//         if (bool === true) {
+//             count++;
+//         }
+//     });
+//     return count;
+// }
+
+
+boolArr = [true, false, true, true, false, true, false];
+
+boolArr2 = [];
+
+console.log(countTrue(boolArr));
+console.log(countTrue(boolArr2));
