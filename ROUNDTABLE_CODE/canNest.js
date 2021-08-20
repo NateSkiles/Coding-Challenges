20fed65dfb53fb45b861cf35822a58e7722851e7
Nathan Skiles <nateskiles94@gmail.com>
2021-08-20 16:07:28 -0700

    8.18

---
+++ ROUNDTABLE_CODE/canNest.js
@@ -0,0 +1,17 @@
+// Create a function that returns true if the first array can be nested inside the second.
+// arr1 can be nested inside arr2 if:
+
+// arr1's min is greater than arr2's min.
+// arr1's max is less than arr2's max.
+
+const arr2 = [1, 4, 8, 7]
+const arr1 = [5, 9, 0]
+
+function canNest(arr1, arr2) {
+    if (Math.min(...arr1) > Math.min(...arr2) || Math.max(...arr1) < Math.max(...arr2)) {
+        return true;
+    }
+    else return false;
+}
+
+console.log(canNest(arr1, arr2))
--- ROUNDTABLE_CODE/firstFactorial.js
+++ ROUNDTABLE_CODE/firstFactorial.js
@@ -1,27 +1,19 @@
 // Take the num parameter being passed and return the factorial of it. 
 // For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
-
-// function firstFactorial(num) {
-//     let result = 1;
-//     for (let i = 2; i <= num; i++) {
-//         result *= i;
-//     }
-//     return result;
-// }
+// n! = n * (n-1)!
 
 function firstFactorial(num) {
-    // Check if less than 0
     if (num < 0) {
-        return - 1;
+        return -1;
     }
-    // If number is 0, its factorial is 1.
+
     else if (num === 0) {
         return 1;
     }
-
+    
     else {
         return (num * firstFactorial(num - 1))
     }
 }
 
-console.log(firstFactorial(4));
+console.log(firstFactorial(12))
